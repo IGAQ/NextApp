e45446e8c13305da03ad1547ed08ac47436c2a05
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-10-12 10:49:17 -0700

    dark mode storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-dark-mode",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,10 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: "black" },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: "red" },
+  },
+};
