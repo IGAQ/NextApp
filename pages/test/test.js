1b1271d8d1cb7c20f1ac726a2a0a7d20964ee68f
Roncyboy <tpaiement@my.bcit.ca>
2022-10-18 03:27:01 -0700

    avatar and tag

---
+++ pages/test/test.js
@@ -0,0 +1,11 @@
+import Tag from "../../comps/base/tags";
+import Profile from "../../comps/base/avatars";
+
+export default function Testing (){
+    return (
+    <div>
+        <Tag bgcolor="red" text="test"></Tag>
+        <Profile clr="deepPurple" AvaPic="AB" size="large"></Profile>
+    </div>
+    )
+}
