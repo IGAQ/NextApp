2caba2ad022c2244ae93528dc8ab9340ba7ffc7e
Ilia Abedian <37903573+iliaamiri@users.noreply.github.com>
2022-10-18 21:01:13 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:8080/:path*',
+          },
+        ]
+  },
 }
 
 module.exports = nextConfig
