29611b8c11b2d8b79fcfb6248b5efadfbf999949
Ilia Abedian <eilia.abedian@yahoo.com>
2022-10-19 13:10:58 -0700

    done

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,17 @@
 module.exports = {
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-dark-mode",
+    "storybook-addon-next"
   ],
   framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  }
 };
