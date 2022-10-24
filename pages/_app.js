1355a1779b7bf9f1cadbab7c0f9b76334d4fbb8c
MadiMagpie <mcafikirwin@gmail.com>
2022-10-23 18:27:57 -0700

    fixes bottom nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import '../styles/globals.css';
+import { MobileNav } from '../components/Organisms/MobileNav/MobileNav';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+    const r = useRouter();
+    function showBottomNav(){
+        if (router.pathname === '/login' || router.pathname === '/newQueery'){
+            return false; 
+        }
+        else{
+            return true;
+        }
+    } 
+
     return ( 
-        <Component {...pageProps} />
+        <>
+            <Component {...pageProps} />
+            {/* {<showBottomNav && MobileNav/>} */}
+            <MobileNav/>
+        </>
     );
 }
 
--- pages/homepage.js
+++ pages/homepage.js
@@ -15,76 +15,12 @@ import axios from 'axios';
 
 export default function Homepage(props){
 
-    function getPageIndex(route) {
-        // switch (route) {
-        //   case '/homepage': return 0;
-        //   case '/resources': return 1;
-        //   case '/notifications': return 2;
-        //   case '/profile': return 3;
-        //   default: return 0;
-        // }
-
-        if (route === '/homepage'){
-            return 0;
-        } else if (route === '/resources'){     
-            return 1;
-        } else if (route === '/notifications'){
-            return 2;
-        } else if (route === '/profile'){
-            return 3;
-        } else{
-            return 0;
-        }
-
-    }
-
-    const handlePageChange = (event, newPage) => {
-        // const path = event.target.getAttribute('value');
-        // console.log("value from event :"+ path);
-        // r.push(path);
-        setPage(newPage);             
-    };
-
-    function closePostModal(){
-        setPostModal(false);
-    }
-
-    function openPostModal(){
-        setPostModal(true);
-    }
-        
     const r = useRouter();
-    const pathname = r.pathname;
-    const currentPage = getPageIndex(pathname);
-    const [page, setPage] = useState(0);
-    const [ postModal, setPostModal] = useState(false);
-        
+    
     return(
         <Wrapper width='0' height='0'>
             <FlexBox justify = 'flex-start' dir = 'column' align = 'flex-start' flex = '0'> Queery of the day </FlexBox>
             <BaseCard posts = {props.posts}/>
-
-            {postModal && <NewPostModal onClick = {closePostModal}/>}
-            {postModal && <ModalBackdrop onClick = {closePostModal}/>}
-
-            <BottomNavigation
-                value={page}
-                onChange={handlePageChange}
-                sx={{
-                    position: 'fixed', bottom: '0', width: '100vw', zindex: 2,
-                    '& .MuiBottomNavigationAction-root, .Mui-selected, svg': {
-                        color: '#131313'},
-                    '& .Mui-selected, .Mui-selected > svg': {
-                        color: '#FF758C',
-                    },
-                                        
-                }}>
-                <BottomNavigationAction value = '/homepage' label="Home" icon={<HomeRoundedIcon /> } onClick={()=>r.push('/homepage')}/>
-                <BottomNavigationAction value = '/resources' label="Resources" icon={<ImportContactsRoundedIcon />} onClick={()=>r.push('/resources')}/>
-                <BottomNavigationAction label=" " icon={<AddRoundedIcon />} onClick = {openPostModal}/>
-                <BottomNavigationAction value = '/notifications' label="Notifications" icon={<NotificationsNoneRoundedIcon />}onClick={()=>r.push('/notifications')}/>
-                <BottomNavigationAction label="Profile" value = '/profile' icon={<PersonOutlineRoundedIcon />}onClick={()=>r.push('/profile')} />
-            </BottomNavigation>
         </Wrapper>
     );
 }
