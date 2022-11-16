d567a60e4c7e9ffb289b41784cc0ed0f43c7db24
MelysaNguyen <mnguyen181@my.bcit.ca>
2022-11-15 20:53:50 -0800

    add actual text/info to about and comm guidelines

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,8 +5,8 @@ import { PostTitle } from '../../components/Molecules/PostTitle';
 import { BackArrow } from '../../components/Atoms/BackArrow';
 import Image from 'next/image';
 
-
 export default function About() {
+
     return (
         <Wrapper align='stretch' justify='center' bgColor='white'>
             <BackArrow />
@@ -22,7 +22,7 @@ export default function About() {
 
                     <Text
                         padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
+                        text='I Got A Queery (IGAQ) provides a safe space for the members of the LGBTQ+ community to ask questions, vent, or share stories about all things LGBTQ+.'
                         size='0.9em' />
 
                     <Text
@@ -31,10 +31,14 @@ export default function About() {
                         size='1em'
                         weight='500' />
 
-                    <Text
-                        padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
-                        size='0.9em' />
+                    <Text padding='0em 2em 0em 2em' text='Our purpose is to help facilitate a safe space for people to find comfort and explore their identities. Users can safely ask questions and find answers regarding all things LGBTQ+ and connect with a community of like-minded users. Our platform provides access to the resources and tools to assist them in learning more about LGBTQ+.' size='0.9em' />
+                    <Text padding='2em 2em 0em 2em' text='Within our platform, members can:' size='0.9em' />
+                    <Text padding='1em 2em 0em 2em' text='- Relate with other members who are dealing with similar problems related to LGBTQ+' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Ask questions relating to LGBTQ+ or about coming out' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Understand their identity or sexuality' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Inspire others with their stories/experience' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Vent/rant about a bad experience' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Learn the different types of LGBTQ+ labels' size='0.9em' />
 
                     <Text
                         padding='1em 2em 1em 2em'
@@ -42,12 +46,10 @@ export default function About() {
                         size='1em'
                         weight='500' />
 
-                    <Text
-                        padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
-                        size='0.9em' />
-
-                    <Spacer axis='vertical' size={40} />
+                    <Text padding='0em 2em 0em 2em' text='Ages 13-19 are the common age group for people to begin exploring their sexuality.' size='0.9em' />
+                    <Text padding='1em 2em 0em 2em' text='We also invite members who are more experienced in order to foster a "mentor" kind of environment, where help and advice are more accessible.' size='0.9em' />
+                    <Text padding='1em 2em 0em 2em' text='As always, we welcome diversity and do not discriminate against any age, race, or religion.' size='0.9em' />
+                    <Spacer axis='vertical' size={90} />
                 </FlexBox>
             </FlexBox>
         </Wrapper>
--- pages/communityGuidelines/index.js
+++ pages/communityGuidelines/index.js
@@ -14,7 +14,7 @@ export default function CommunityGuidelines() {
                 <FlexBox align='flex-start' bgColor='white'>
                     <Text
                         padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
+                        text='We want IGAQ to be an authentic and safe place for our users to explore their sexualities. Help us foster this community by treating everyone with respect. Don’t post hate, troll, or spam. To meet the collective goals of our community, it’s important that all members feel safe and supported. To help everyone have the best possible experience, please take a look at our community guidelines:'
                         size='0.9em' />
 
                     <Text
@@ -23,10 +23,11 @@ export default function CommunityGuidelines() {
                         size='1em'
                         weight='500' />
 
-                    <Text
-                        padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
-                        size='0.9em' />
+
+                    <Text padding='0em 2em 0em 2em' text='- Feel free to post questions and start discussions on any topics relating to your sexuality, identity, or coming-out stories.' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Treat others as you would treat them in real life' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Be polite and communicate with respect' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Respect the privacy of other community members' size='0.9em' />
 
                     <Text
                         padding='1em 2em 1em 2em'
@@ -34,10 +35,13 @@ export default function CommunityGuidelines() {
                         size='1em'
                         weight='500' />
 
-                    <Text
-                        padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
-                        size='0.9em' />
+                    <Text padding='0em 2em 0em 2em' text='- Don’t share personal or private information' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Don’t use homophobic slurs' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Don’t post fake stories to gain sympathy or pity' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Don’t post promotional content' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Understand their identity or sexuality' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Don’t post explicit, rude, or aggressive content' size='0.9em' />
+                    <Text padding='0.25em 2em 0em 2em' text='- Personal attacks, trolling, and abuse will not be tolerated' size='0.9em' />
 
                     <Text
                         padding='1em 2em 1em 2em'
@@ -47,7 +51,7 @@ export default function CommunityGuidelines() {
 
                     <Text
                         padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
+                        text='We want to make sure the posts here are authentic. An authentic environment creates an authentic community, and that’s why we don’t allow fake stories or troll posts on our platform that will negatively affect the reputation of I Got A Queery.'
                         size='0.9em' />
 
                     <Text
@@ -58,7 +62,7 @@ export default function CommunityGuidelines() {
 
                     <Text
                         padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
+                        text='We strive to make I Got A Queery a safe space. We will remove content that we deem as harmful to people’s privacy and security. Threats, bullying, and hate speech are prohibited.'
                         size='0.9em' />
 
                     <Text
@@ -69,7 +73,9 @@ export default function CommunityGuidelines() {
 
                     <Text
                         padding='0em 2em 0em 2em'
-                        text='a bunch of text gneajngkea sj xsaxsaxasageagneajkgnejakgnjekangjnekjnjkangjekngkejsxsaxaxsaxsaxsaxsaxsaxnegkangneajgneakngejakgnksa'
+                        text='In the event of someone violating these guidelines, we will take action to protect other members of the group. This might include a warning or in the event of extreme or repeated behavior, the user may be banned from the platform. 
+
+                        If you experience or witness any behavior that doesn’t follow our community guidelines, please contact us at ReportIGAQ@gmail.com. All reports are kept confidential.'
                         size='0.9em' />
 
                     <Spacer axis='vertical' size={40} />
