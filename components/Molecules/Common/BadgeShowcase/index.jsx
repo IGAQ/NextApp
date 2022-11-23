0107664b39b9091391d7a07eeeed500146a4b951
MelysaNguyen <mnguyen181@my.bcit.ca>
2022-11-22 21:45:20 -0800

    change profile frontend

--- components/Molecules/Common/BadgeShowcase/index.jsx
+++ components/Molecules/Common/BadgeShowcase/index.jsx
@@ -1,24 +1,26 @@
 import { Badge } from '../../../Atoms/Common/Badge';
 import { FlexBox } from '../../../../styles/globals';
-import {Text} from '../../../Atoms/Common/Text';
-import {Spacer} from '../../../Atoms/Common/Spacer';
+import { Text } from '../../../Atoms/Common/Text';
+import { Spacer } from '../../../Atoms/Common/Spacer';
 
 
 export function BadgeShowcase() {
     return (
-        <FlexBox dir = "column" padding = "15px 0 0 0 " margin = '0 20px'>
-            <Text size = "1rem" weight = "600" text = "My badge showcase"></Text>
-            <Spacer axis="vertical" size={15}/>
-            <FlexBox dir = "row" flexWrap = "wrap" gap = "1rem">
-                {/* badges.map((badge) => <Badge badge = {badge} />) */}
-                <Badge/>
-                <Badge/>
-                <Badge/>
-                <Badge/>
-                <Badge/>
-                <Badge/>
-                <Badge/>
-                <Badge/>
+        <FlexBox padding='0 3rem 0 3rem' align="stretch" justify="center" bgColor="#DFEEFF">
+            <FlexBox dir="column" padding='1rem 2rem 1rem 2rem' margin='5px 0px 15px 0px' bgColor='#ffffff' borderRadius='0.5rem'>
+                <Text size="1rem" weight="600" text="My Badge Showcase"></Text>
+                <Spacer axis="vertical" size={15} />
+                <FlexBox dir='row' flexWrap='wrap' gap='1.5rem' bgColor='#ffffff' borderRadius='0.5rem'>
+                    {/* badges.map((badge) => <Badge badge = {badge} />) */}
+                    <Badge />
+                    <Badge />
+                    <Badge />
+                    <Badge />
+                    <Badge />
+                    <Badge />
+                    <Badge />
+                    <Badge />
+                </FlexBox>
             </FlexBox>
         </FlexBox>
     );
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,36 +1,32 @@
-import {FlexBox} from '../../styles/globals';
-import {Spacer} from '../../components/Atoms/Common/Spacer';
-import {HamburgerIcon} from '../../components/Atoms/Common/Icons/HamburgerIcon';
-import {PostTitle} from '../../components/Molecules/Post/PostTitle';
-import {Text} from '../../components/Atoms/Common/Text';
-import {Button} from '../../components/Atoms/Common/Buttons/Button';
-import {ProfileIcon} from '../../components/Atoms/Common/Icons/ProfileIcon';
-import {BadgeShowcase} from '../../components/Molecules/Common/BadgeShowcase';
-import {ProfileBio} from '../../components/Molecules/Common/ProfileBio';
+import { FlexBox } from '../../styles/globals';
+import { Spacer } from '../../components/Atoms/Common/Spacer';
+import { HamburgerIcon } from '../../components/Atoms/Common/Icons/HamburgerIcon';
+import { PostTitle } from '../../components/Molecules/Post/PostTitle';
+import { Text } from '../../components/Atoms/Common/Text';
+import { Button } from '../../components/Atoms/Common/Buttons/Button';
+import { BadgeShowcase } from '../../components/Molecules/Common/BadgeShowcase';
+import { ProfileBio } from '../../components/Molecules/Common/ProfileBio';
 
 export default function Profile(props) {
     return (
-        <FlexBox align="stretch" justify="center" bgColor="#A5CEFF">
-            <HamburgerIcon/>
-            <PostTitle title="Profile"/>
-            <Spacer axis="vertical" size={45}/>
+        <FlexBox align="stretch" justify="center" bgColor="#DFEEFF">
+            <HamburgerIcon />
+            <PostTitle title="Profile" />
+            <Spacer axis="vertical" size={45} />
             <FlexBox padding="25px 0px" align="center" bgColor="#DFEEFF">
-                <ProfileIcon marginRight='0'/>
-                <Text text="John" weight="600" size="1.5rem"/>
-                <Spacer axis="vertical" size={15}/>
-                <ProfileBio/>
-                <Spacer axis="vertical" size={25}/>
+                <ProfileBio />
+                <Spacer axis="vertical" size={25} />
                 <FlexBox>
-                    <Button size="long" label="View post history"/>
-                    <Spacer axis="vertical" size={25}/>
-                    <Button size="long" label="Saved resources"/>
-                    <Spacer axis="vertical" size={25}/>
-                    <Button size="long" label="Saved posts"/>
+                    <Button size="long" label="View Post History" />
+                    <Spacer axis="vertical" size={25} />
+                    <Button size="long" label="Saved Resources" />
+                    <Spacer axis="vertical" size={25} />
+                    <Button size="long" label="Saved Posts" />
                 </FlexBox>
             </FlexBox>
-            <Spacer axis="vertical" size={15}/>
-            <BadgeShowcase/>
-            <Spacer axis="vertical" size={55}/>
+            <Spacer axis="vertical" size={15} />
+            <BadgeShowcase />
+            <Spacer axis="vertical" size={55} />
         </FlexBox>
 
 
